93115f1b8c4fc8c7411b914e77228b9a81e941c4
Hpollard003 <hpollard003@gmail.com>
2021-04-27 17:02:17 -0400

    Fixing and planning next steps with comments

--- HEPHAESTUS.js
+++ HEPHAESTUS.js
@@ -1,6 +1,8 @@
 // global variables
 const mediumUrl = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple'
 const hardUrl = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=hard&type=multiple'
+
+
 //fetch for medium
 const mediumQA = () => {
     fetch(mediumUrl)
@@ -8,13 +10,19 @@ const mediumQA = () => {
     .then(data => console.log(data))
 } 
 mediumQA()
+
+
+//fetch for hard
 const hardQA = () => {
     fetch(hardUrl)
     .then(resp => resp.json())
     .then(data => console.log(data))
 }
 hardQA()
-//fetch for hard
-//event listeners
-//popup
 
+
+//event listeners
+//correct answer
+//dropdown for selecting difficulty 
+//popup
+//render questions to webpage
