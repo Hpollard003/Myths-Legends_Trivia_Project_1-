0b8d5a109adbad3af73e0bf5ded2bc4d6ba6acad
Hpollard003 <hpollard003@gmail.com>
2021-04-27 16:58:11 -0400

    First Fetch Success(difficulty hard)

--- HEPHAESTUS.js
+++ HEPHAESTUS.js
@@ -1,5 +1,6 @@
 // global variables
 const mediumUrl = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple'
+const hardUrl = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=hard&type=multiple'
 //fetch for medium
 const mediumQA = () => {
     fetch(mediumUrl)
@@ -7,6 +8,12 @@ const mediumQA = () => {
     .then(data => console.log(data))
 } 
 mediumQA()
+const hardQA = () => {
+    fetch(hardUrl)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
+hardQA()
 //fetch for hard
 //event listeners
 //popup
