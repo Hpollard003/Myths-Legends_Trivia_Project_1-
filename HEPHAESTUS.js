fe40ab95b32d61bfe51a1fa0b6eea10b355f2b2d
Hpollard003 <hpollard003@gmail.com>
2021-04-27 16:54:48 -0400

    First Fetch Success(difficulty medium)

--- HEPHAESTUS.js
+++ HEPHAESTUS.js
@@ -1,5 +1,12 @@
 // global variables
+const mediumUrl = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple'
 //fetch for medium
+const mediumQA = () => {
+    fetch(mediumUrl)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+} 
+mediumQA()
 //fetch for hard
 //event listeners
 //popup
