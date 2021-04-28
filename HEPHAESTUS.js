d6bfa3d109dbb017ae272716362fc9e61fc0c66a
Hpollard003 <hpollard003@gmail.com>
2021-04-27 22:52:06 -0400

    now on to tackling that dropdown so i can change the difficulty

--- HEPHAESTUS.js
+++ HEPHAESTUS.js
@@ -1,8 +1,9 @@
+//dropdown for selecting difficulty 
+//on sumbit change ${difficulty} 
 // global variables
 const apiUrl = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple'
-
-
-
+const hard = 'hard'
+const medium = 'medium'
 //fetch for api data
 fetch(apiUrl)
     .then(resp => resp.json())
@@ -11,32 +12,32 @@ fetch(apiUrl)
 
 function renderTrivia(data) {
     data.results.forEach(dataArr => {
-        renderTriviaQ(dataArr)})
+        renderTriviaQnA(dataArr)})
 }
+function renderTriviaQnA(data){
+    const newElement = document.createElement("ol")
+    const collection = document.querySelector('.questions')
+    const options = [...data.incorrect_answers,data.correct_answer]
 
-function renderTriviaQ(data){
-    const newElement = document.createElement("ul")
-    const collection = document.getElementById('questions')
-    const options = [...data.incorrect_answers,data.correct_answer] 
     newElement.innerHTML = `
-    <div class="my-5" id="1">
-    <p class="lead font-weight-heavy text-info" id="q1">Question
-    <li class='text-white'>${data.question}</li></p>
+    <div class="fab my-4" id="1">
+    <p class="lead font-weight-heavy text-info" id="q1">Question  
+    <p class='text-white'>${data.question}</p></p>
     <div class="form-check my-3 text-info-50" id="c1">
-        <input type="radio" name='q1' value="A" checked>
-        <label class="form-check-label"> ${data.correct_answer} </label>
+        <input type="radio" name='q1' value="A" >
+        <label class="form-check-label text-white"> ${options[2]} </label>
     </div>
     <div class="form-check my-3 text-info-50">
         <input type="radio" name='q1' value="B" >
-        <label class="form-check-label"> ${options[0]}</label>
+        <label class="form-check-label text-white"> ${options[0]}</label>
     </div>
     <div class="form-check my-3 text-info-50">
         <input type="radio" name='q1' value="C" >
-        <label class="form-check-label"> ${options[1]} </label>
+        <label class="form-check-label text-white"> ${options[3]} </label>
     </div>
     <div class="form-check my-3 text-info-50">
         <input type="radio" name='q1' value="D" >
-        <label class="form-check-label"> ${options[2]} </label>
+        <label class="form-check-label text-white"> ${options[1]} </label>
     </div>
 </div>
     `
@@ -46,7 +47,7 @@ function renderTriviaQ(data){
 //func renderRadio(questionObj) //including options and correct answer
     //radio = createElement(radio)
     //add html 
-    ///radio.addEventListener(click) 
+    ///radio.addEventpstener(cpck) 
         //if(value == correct_answer){
            // correctCount++ 
            // event.target.className = 'correct'
@@ -54,18 +55,12 @@ function renderTriviaQ(data){
 
 //on submit 
     //querySelectorAll (css suedo selector for selected radio buttons)
-    // iterate over the node list, check for attribute indicting if it was corect or not
+    // iterate over the node pst, check for attribute indicting if it was corect or not
     //then apply css red or green for each element
     //render correctCount
 
-//event listeners
+//event psteners
 //correct answer
-//dropdown for selecting difficulty 
-//on sumbit change ${difficulty} 
-// const activities = document.getElementById('dropdownMenuButton');
 
-// activities.addEventListener('click', (e) => {
-//   console.log(e);
-// });
 //popup
 //render questions to webpage
