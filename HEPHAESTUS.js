ed9dae62349f52c353c4619e2117d0edfa905e29
Hpollard003 <hpollard003@gmail.com>
2021-04-27 22:23:37 -0400

    Ok had to rethink things but this is better

--- HEPHAESTUS.js
+++ HEPHAESTUS.js
@@ -1,48 +1,71 @@
 // global variables
-const mediumUrl = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple'
-const hardUrl = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=hard&type=multiple'
+const apiUrl = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple'
 
 
-//fetch for medium
-fetch(mediumUrl)
+
+//fetch for api data
+fetch(apiUrl)
     .then(resp => resp.json())
-    .then(data => renderMedium(data))
+    .then(data => renderTrivia(data))
     .catch(error => console.error(error))
 
-function renderMedium(data) {
-    data.results.forEach(med => {
-        renderMediumQ(med)})
-}
-function renderMediumQ(data){
-    renderMediumA(data)
-    const questions = data.question
-    console.log(questions)
-}
-function renderMediumA(data) {
-    console.log(data.correct_answer)
-    console.log(data.incorrect_answers)
+function renderTrivia(data) {
+    data.results.forEach(dataArr => {
+        renderTriviaQ(dataArr)})
 }
 
-renderMedium()
+function renderTriviaQ(data){
+    const newElement = document.createElement("ul")
+    const collection = document.getElementById('questions')
+    const options = [...data.incorrect_answers,data.correct_answer] 
+    newElement.innerHTML = `
+    <div class="my-5" id="1">
+    <p class="lead font-weight-heavy text-info" id="q1">Question
+    <li class='text-white'>${data.question}</li></p>
+    <div class="form-check my-3 text-info-50" id="c1">
+        <input type="radio" name='q1' value="A" checked>
+        <label class="form-check-label"> ${data.correct_answer} </label>
+    </div>
+    <div class="form-check my-3 text-info-50">
+        <input type="radio" name='q1' value="B" >
+        <label class="form-check-label"> ${options[0]}</label>
+    </div>
+    <div class="form-check my-3 text-info-50">
+        <input type="radio" name='q1' value="C" >
+        <label class="form-check-label"> ${options[1]} </label>
+    </div>
+    <div class="form-check my-3 text-info-50">
+        <input type="radio" name='q1' value="D" >
+        <label class="form-check-label"> ${options[2]} </label>
+    </div>
+</div>
+    `
+    collection.appendChild(newElement); 
+}
 
-//fetch for hard
-// fetch(hardUrl)
-//     .then(resp => resp.json())
-//     .then(quiz => renderHard(quiz))
-//     .catch(error => console.error(error))
+//func renderRadio(questionObj) //including options and correct answer
+    //radio = createElement(radio)
+    //add html 
+    ///radio.addEventListener(click) 
+        //if(value == correct_answer){
+           // correctCount++ 
+           // event.target.className = 'correct'
+//}
 
-// function renderHard(data) {
-//     data.results.forEach(hard => {
-//         renderHardQ(hard)})
-//     }
-// function renderHardQ(data){
-//     const questions = data.question
-//     console.log(questions)
-//     }
+//on submit 
+    //querySelectorAll (css suedo selector for selected radio buttons)
+    // iterate over the node list, check for attribute indicting if it was corect or not
+    //then apply css red or green for each element
+    //render correctCount
 
-// renderHardQ()
 //event listeners
 //correct answer
 //dropdown for selecting difficulty 
+//on sumbit change ${difficulty} 
+// const activities = document.getElementById('dropdownMenuButton');
+
+// activities.addEventListener('click', (e) => {
+//   console.log(e);
+// });
 //popup
 //render questions to webpage
