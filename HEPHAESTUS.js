e0daa45445723b257d69acdd9ebf85881d3a624c
Hpollard003 <hpollard003@gmail.com>
2021-04-30 23:39:13 -0400

    commit tree 94919d057d37d05610a79b7bacc3f6c5450fdada

--- HEPHAESTUS.js
+++ HEPHAESTUS.js
@@ -1,100 +1,25 @@
 // global variables
+const click = document.getElementsByClassName('answer')
+const collection = document.querySelector('.questions')
 const answers = ['C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C']
 const btn = document.querySelector('.trivia-form')
 const newScore = document.querySelector('.score')
-
-
-//fetching the api data
-function fetchApis(level){
-fetch(`https://opentdb.com/api.php?amount=10&category=20&difficulty=${level}&type=multiple`)
-    .then(resp => resp.json())
-    .then(data => renderTrivia(data))
-    .catch(error => console.error(error))
-//render data from API
-function renderTrivia(data) {
-    data.results.forEach((dataArr, index) => {
-        renderTriviaQnA(dataArr, index)
-    })
-
-}}
-function renderTriviaQnA(data, index) {
-    // console.log(index)
-    const newElement = document.createElement("ol")
-    newElement.innerHTML = ""
-    const collection = document.querySelector('.questions')
-    let options = [...data.incorrect_answers, data.correct_answer]
-    // options = shuffle(options)
-    //options.forEach()
-        // create div, input and label
-        // attach event listen to input 
-        //input.addEventListener()
-            //if(target.value == data.correct_answer)
-                //correct++
-
-    // console.log(data.correct_answer)
-    const answers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    newElement.innerHTML = `
-    <div class=" fab m-4 p-3 card bg-dark bg-gradient rounded" style="width: 80rem" id="1">
-        <p class="lead font-weight-heavy text-success card-title" id="q1">${data.question}</p>  
-
-        <div class="answer form-check my-3 " id="c1">
-            <input type="radio" name='answer${answers[index]}' value="A" >
-            <label class="form-check-label "> ${options[2]} </label>
-        </div>
-        <div class="answer form-check my-3 ">
-            <input type="radio" name='answer${answers[index]}' value="B" >
-            <label class="form-check-label "> ${options[0]} </label>
-        </div>
-        <div class="answer form-check my-3 ">
-            <input type="radio" name='answer${answers[index]}' value="C" >
-            <label class="form-check-label "> ${options[3]} </label>
-        </div>
-        <div class="answer form-check my-3 ">
-            <input type="radio" name='answer${answers[index]}' value="D" >
-            <label class="form-check-label "> ${options[1]} </label>
-        </div>
-    </div>
-    `
-
-    collection.appendChild(newElement);
-    // options.reverse()
-    // return shuffle(options)
-}
-
-//shuffle correct answers(Special thanks to the creators of the Fisher-Yates (aka Knuth) Shuffle.)
-function shuffle(answers) {
-    var currentIndex = answers.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = answers[currentIndex];
-      answers[currentIndex] = answers[randomIndex];
-      answers[randomIndex] = temporaryValue;
-    }
-
-    return answers;
-  }
 
 //event listeners
 btn.addEventListener('submit', e => {
     const userA = [btn.answer1.value, btn.answer2.value, btn.answer3.value, btn.answer4.value, btn.answer5.value, btn.answer6.value, btn.answer7.value, btn.answer8.value, btn.answer9.value, btn.answer10.value];
     let score = 0;
-
     userA.forEach((answr, ind) => {
         if (answr === answers[ind]) {
+            //.innerText === data.correct_answer
             score += 10;
         }
     })
     e.preventDefault();
     // console.log(score)
     scrollTo(0, 0)
-
+    // console.log(data)
+    
     let animamation = 0
     const stopWatch = setInterval(() => {
         newScore.innerHTML = `
@@ -104,12 +29,88 @@ btn.addEventListener('submit', e => {
             clearInterval(stopWatch)
         } else {
             animamation++
-        } 
+        }
     }, 50)
 
 
 })
-const click = document.getElementsByClassName('answer')
+
+//fetching the api data
+function fetchApis(level) {
+    fetch(`https://opentdb.com/api.php?amount=10&category=20&difficulty=${level}&type=multiple`)
+        .then(resp => resp.json())
+        .then(data => renderTrivia(data))
+        .catch(error => console.error(error))
+    //render data from API
+    function renderTrivia(data) {
+        data.results.forEach((dataArr, index) => {
+            renderTriviaQnA(dataArr, index)
+        })
+
+    }
+}
+function renderTriviaQnA(data, index) {
+    // console.log(index)
+    const newElement = document.createElement("ol")
+    let options = [...data.incorrect_answers, data.correct_answer]
+    options = shuffle(options)
+    //options.forEach()
+    // create div, input and label
+    // attach event listen to input 
+    //input.addEventListener()
+    //if(target.value == data.correct_answer)
+    //correct++
+    newElement.innerHTML = "<div>  </div>"
+    
+    console.log(data.correct_answer)
+    const answers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    newElement.innerHTML = `
+    <div class=" fab m-4 p-3 card bg-dark bg-gradient rounded" style="width: 80rem" id="1">
+    <p class="lead font-weight-heavy text-success card-title" id="q1">${data.question}</p>  
+    
+    <div class="answer form-check my-3 " id="c1">
+    <input type="radio" name='answer${answers[index]}' value="A" >
+    <label class="form-check-label "> ${options[2]} </label>
+    </div>
+    <div class="answer form-check my-3 ">
+    <input type="radio" name='answer${answers[index]}' value="B" >
+    <label class="form-check-label "> ${options[0]} </label>
+    </div>
+    <div class="answer form-check my-3 ">
+    <input type="radio" name='answer${answers[index]}' value="C" >
+    <label class="form-check-label "> ${options[3]} </label>
+    </div>
+    <div class="answer form-check my-3 ">
+    <input type="radio" name='answer${answers[index]}' value="D" >
+    <label class="form-check-label "> ${options[1]} </label>
+    </div>
+    </div>
+    `
+    collection.appendChild(newElement);
+    // options.reverse()
+    
+}
+
+//shuffle correct answers(Special thanks to the creators of the Fisher-Yates (aka Knuth) Shuffle.)
+function shuffle(answers) {
+    var currentIndex = answers.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = answers[currentIndex];
+        answers[currentIndex] = answers[randomIndex];
+        answers[randomIndex] = temporaryValue;
+    }
+
+    return answers;
+}
+
 
 
 
@@ -122,8 +123,8 @@ const dropdown = document.getElementById('dropdown')
 dropdown.addEventListener('change', (e) => {
     const level = e.target.value
     fetchApis(level)
-})  
-// let level    
+})
+// let level
 
 
 //correct answer
